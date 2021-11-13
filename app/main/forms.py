07129ec80cf7291853223892c9209c3cdbf17108
mnubi <rodger.kamau@student.moringaschool.com>
2021-11-13 21:07:57 +0300

    created form to collect user data

---
+++ app/main/forms.py
@@ -0,0 +1,14 @@
+from wtforms import StringField,TextAreaField, SubmitField, SelectField 
+from wtforms.validators import Required, Email, Length
+from flask_wtf import FlaskForm
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell Us About Yourself...',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class UpdateProfileForm(FlaskForm):
+    name = StringField('Name', validators=[Required(), Length(1, 64)])
+    username = StringField('Username', validators=[Required(), Length(1, 64)])
+    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    bio = TextAreaField('About...', validators=[Required(), Length(1, 100)])
+    submit = SubmitField('Submit')
