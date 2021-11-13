c3b26d2e2d6d2c4ab277502af820870725c102d5
mnubi <rodger.kamau@student.moringaschool.com>
2021-11-13 20:26:45 +0300

    added navbar to app

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from . import db,login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
